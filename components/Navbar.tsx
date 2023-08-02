427e70090c2944751fa53174b3e420d6d1e0cf7c
nazarilkiv <bruem35@gmail.com>
2023-08-02 16:49:47 +0300

    add NextAuth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,9 +3,10 @@ import Link from "next/link";
 import Image from "next/image";
 import { NavLinks } from "@/constants/index";
 import AuthProviders from "@/components/AuthProviders";
+import { getCurrentUser } from "@/lib/session";
 
-const Navbar = () => {
-  const session = {};
+const Navbar = async () => {
+  const session = await getCurrentUser;
 
   return (
     <nav className="flexBetween navbar">
